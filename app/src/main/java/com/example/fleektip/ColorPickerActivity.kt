884fa0389a92fdecf2c7be37db2f3e90f4481c58
alexander0949 <delacruz09@gmail.com>
2025-11-13 11:16:35 +0800

    ADDED NAIL EXTENSION + COLORS

--- app/src/main/java/com/example/fleektip/ColorPickerActivity.kt
+++ app/src/main/java/com/example/fleektip/ColorPickerActivity.kt
@@ -130,6 +130,7 @@ class ColorPickerActivity : AppCompatActivity() {
 
         for ((id, colorName) in colors) {
             findViewById<Button>(id).setOnClickListener {
+
                 //Nail Polish Mode
                 if (isNailPolishMode) {
                     // Insert Logic for Nail Polish Here
@@ -154,6 +155,91 @@ class ColorPickerActivity : AppCompatActivity() {
 
                 // Insert Logic for Nail Length + Color Here
 
+                val key = selectedColor + "-" + selectedNailLength
+
+                when (key) {
+                    //red
+                    "red-short" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "fba3cef5-2a77-44e9-bfe1-dae6e209822b")
+                        startActivity(intent)
+                    }
+                    "red-medium" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "e4fed180-be9b-4386-a000-54aa3da58328")
+                        startActivity(intent)
+                    }
+                    "red-long" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "70da906e-cf96-4ee2-8cff-ee62fd7de55b")
+                        startActivity(intent)
+                    }
+                    //blue
+                    "blue-short" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "1a855963-a8a7-4616-b3ab-07e0f695950d")
+                        startActivity(intent)
+                    }
+                    "blue-medium" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "54ffcd95-3c1c-4693-8114-06441cd2112d")
+                        startActivity(intent)
+                    }
+                    "blue-long" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "ad50cfe0-306d-46d3-b542-89bddc7c8c43")
+                        startActivity(intent)
+                    }
+                    //white
+                    "white-short" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "499fd305-8373-4e31-9d6b-98a9f72f2e18")
+                        startActivity(intent)
+                    }
+                    "white-medium" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "6d1f5bef-5368-4032-b71d-cab34237a57a")
+                        startActivity(intent)
+                    }
+                    "white-long" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "467afe97-707d-433c-ac9c-964db83275d4")
+                        startActivity(intent)
+                    }
+                    //brown
+                    "brown-short" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "b1a45548-82ad-4e29-9cfe-975fb2ee1421")
+                        startActivity(intent)
+                    }
+                    "brown-medium" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "f6adca42-c797-40c6-b877-86715f869b29")
+                        startActivity(intent)
+                    }
+                    "brown-long" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "68afa8b9-5869-4ba2-a92b-6e049f9fc26a")
+                        startActivity(intent)
+                    }
+                    //pink
+                    "pink-short" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "0199a3bc-cfca-442b-9936-97bfdb72aa8c")
+                        startActivity(intent)
+                    }
+                    "pink-medium" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "58049e73-d623-48a3-b1f4-c152d6cf959e")
+                        startActivity(intent)
+                    }
+                    "pink-long" -> {
+                        val intent = Intent(this, NailArtActivity::class.java)
+                        intent.putExtra("PUSH_LENS", "0acffcd2-747c-4a7d-aeaf-fdfb3626058a")
+                        startActivity(intent)
+                    }
+                }
+
                 val resultIntent = Intent().apply {
                     putExtra("selectedColor", colorName)
                     putExtra("nailLength", selectedNailLength)
@@ -164,6 +250,7 @@ class ColorPickerActivity : AppCompatActivity() {
             }
         }
     }
+
 
     private fun highlightLengthButtons(selected: Button, other1: Button, other2: Button) {
         selected.setBackgroundTintList(getColorStateList(R.color.pink_light))
